2e25c65644f77c182aca3925e523a348acfd3ee0
Jesus <jgalviscontreras38@gmail.com>
2024-04-01 21:13:18 -0500

    arreglo 22

--- ejercicios/solo_js/ejercicio22.js
+++ ejercicios/solo_js/ejercicio22.js
@@ -1,15 +1,14 @@
 // Escribir un programa que muestre el pago de una llamada telefónica sabiendo que cada 
 // minuto cuesta $355 pesos y un IVA 20%
 
-minutos = Math.ceil;
 
-let minutos = parseFloat(prompt('Ingrese el numero de minutos que gasto en su llamada telefonica, recuerde que asi no halla cumplido todo el minuto se cuenta completo'))
+let minutos = parseFloat(prompt('Ingrese el numero de minutos que gasto en su llamada telefónica, recuerde que así no haya cumplido todo el minuto se cuenta completo'));
 
 const operacion = minutos => {
-    costo= minutos * 335;
-    iva= costo * 0.2;
+    costo = Math.ceil(minutos) * 335;
+    iva = costo * 0.2;
     total = costo + iva;
-    alert(`el costo de su llamada es de ${total}`);
+    alert(`El costo de su llamada es de ${total}`);
 }
 
 operacion(minutos);
